78a789ae31474350b5af9177f843af8dd443dd31
Vikrant Koundal <koundalvikrant111@gmail.com>
2025-04-23 06:05:35 +0000

    Variables

---
+++ Javascript_Basics/Variables.js
@@ -0,0 +1,15 @@
+//Variables
+
+const userName = "Kana";
+var userEmail = "rana@gmail.com";
+let age = 20;
+ age = 30;
+//let age = 30; ❌
+
+// Console in the form of Table and in One Line
+console.log(userName +" " +userEmail +" " +age)
+// console.log(+userName +.....) because + convert into Number ❌ 
+console.table([userName, userEmail, age])
+
+//### alert (3+3) This is only run in Browser(inspect Console) , you can not run in NodeJs ❌
+console.log(3+3)
