41842aefab7e8ad0fd9b2a3c283d98bcd52697b5
Vikrant Koundal <koundalvikrant111@gmail.com>
2025-04-24 18:24:23 +0000

    Primitive data Types

---
+++ Javascript_Basics/Data_Types/primitive.js
@@ -0,0 +1,27 @@
+// Data Types - define the type of value variable can hold
+/* Primitive Data Type- which hold single value, immutable */
+
+//number
+let age = 20;
+
+//string
+let userName = "Rana";
+
+//bollean - large number
+let isLoggedin = false;
+
+//bigint - al large max integer / number
+let largeNumber = 5645665545454;
+
+//null - standalone value
+let box = null;
+
+//undefined - not assign the value to the variable
+let userId ;
+console.log(userId);
+console.log(box);
+
+// type of
+console.log(typeof null) // object (because it is an javascript language error)
+console.log(typeof undefined);
+
