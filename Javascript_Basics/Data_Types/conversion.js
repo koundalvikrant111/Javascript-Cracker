cfd38b27aaa12348b63a58058c10c1f11fd9cd7b
Vikrant Koundal <koundalvikrant111@gmail.com>
2025-04-24 18:24:47 +0000

    Conversions in Javascript

---
+++ Javascript_Basics/Data_Types/conversion.js
@@ -0,0 +1,26 @@
+  //Conversions 
+
+let age = 20;
+let height = "";
+let convertString = String(age)
+console.log(convertString); //20
+console.log(typeof convertString);  //string
+
+let convertLength = String(height)
+console.log(convertLength); //
+
+let convertBollean = Boolean(age)
+console.log(convertBollean); //true
+
+let convertHeight = Boolean(height)
+console.log(convertHeight); //false
+
+let codeNo = "25abc";
+let covertNumberabc = Number(codeNo);
+console.log(covertNumberabc);    //Nan 
+
+
+
+
+
+ 
