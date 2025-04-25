2dbfc68778d80a54c39c4148bc38af4f6e79da1b
Vikrant Koundal <koundalvikrant111@gmail.com>
2025-04-25 18:56:24 +0000

    operators

---
+++ Javascript_Basics/Data_Types/operations.js
@@ -0,0 +1,40 @@
+   //Operations
+
+let value = 3
+let negValue = -value
+console.log(negValue);  //-3
+
+console.log(3+3);  //6
+console.log(3-3);  //0
+console.log(2*2);  //4
+console.log(2**3); //8
+console.log(2%3); //2
+console.log(2/3); //0.6666
+
+//conctatinate
+let str1 = "Raju"
+let str2 = "Kulcha"
+let str3 = str1 +" "+ str2
+console.log(str3); //Raju Kulcha
+
+console.log("1" + 2); // 12
+console.log(1+"2"); // 12
+console.log("2" + 2 +1);  // 221
+console.log(2+1+"5");     // 35
+console.log(2+"5"+1);     // 251
+
+
+console.log(true);  // true
+console.log(+true); // 1 
+// console.log(true+);   give a error
+
+
+
+
+
+
+
+
+
+
+
